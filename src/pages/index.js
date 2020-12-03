2c684257bdaec99063e5d3ef6a5f756c1f1f2238
BJ Firmacion <bj.firmacion@gmail.com>
2020-12-03 21:24:45 +0800

    add parallax section dividers in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import Layout from '../components/layout/Layout';
 import Header from '../components/sections/Header';
 import About from '../components/sections/About';
+import styles from './index.module.scss';
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <About />
+      <div className={`${styles.parallax} ${styles.parallaxA}`} />
+      <About />
+      <div className={`${styles.parallax} ${styles.parallaxB}`} />
     </Layout>
   )
 }
