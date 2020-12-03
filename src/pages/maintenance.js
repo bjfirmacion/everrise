32fce8d542d46cf460b599f7755184eb845617d8
BJ Firmacion <bj.firmacion@gmail.com>
2020-12-03 21:04:12 +0800

    added maintenance page

---
+++ src/pages/maintenance.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Link} from 'gatsby';
+import Layout from '../components/layout/Layout';
+import TextButton from '../components/buttons/TextButton';
+import styles from './maintenance.module.scss';
+
+export default function Maintenance() {
+  return (
+    <Layout>
+      <section className={styles.maintenance}>
+        <div className={`container`}>
+          <div className={styles.textbox}>
+
+            <h1 className={`heading-primary`}>
+              <span className={`heading-primary--main`}><i className={`fas fa-tools`}></i> This page is under construction</span>
+            </h1>
+            <p className={`paragraph ${styles.text}`}>We are currently building the content for this page. Please check back at a later time.</p>
+            <Link to="/">
+              <TextButton>Return to home page &rarr;</TextButton>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
