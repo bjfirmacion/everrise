fd5abca04cd37d0072ca572911055b31310a9c5c
BJ Firmacion <bj.firmacion@gmail.com>
2020-12-14 14:42:03 +0800

    converted hero image of header in homepage to gatsby-background-image

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image'
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 import styles from './Header.module.scss';
 import PrimaryButton from '../components/PrimaryButton';
 
+const getImages = graphql`
+  {
+  file(relativePath: {eq: "hero-cropped.jpg"}) {
+    childImageSharp {
+      fluid(quality: 100) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
+
 function Header() {
+  const data = useStaticQuery(getImages);
+
   return (
-    <header className={styles.header}>
+    <BackgroundImage
+      Tag={`header`}
+      fluid={data.file.childImageSharp.fluid}
+      className={styles.header}
+    >
       <div className="container">
         <div className={styles.textbox}>
           <h1 className="heading-primary">
@@ -17,9 +37,9 @@ function Header() {
               <PrimaryButton>Contact Us</PrimaryButton>
             </AnchorLink>
           </div>
-          </div>
         </div>
-    </header>
+      </div>
+    </BackgroundImage>
   )
 }
 
